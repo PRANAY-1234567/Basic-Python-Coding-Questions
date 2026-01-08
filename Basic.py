ffe14c4029ecdb4b6b55cd3b0cc24ba4aef2e9b9
Pranay <pranayvjadhao197@gmail.com>
2026-01-08 22:46:10 +0530

    Initial commit

---
+++ Basic.py
@@ -0,0 +1,25 @@
+#Reverse a string
+# s = "Pranay"
+# rev = ""
+
+# for i in s:
+#     rev = i + rev
+
+# print(rev)
+
+#Palindrome
+# s = "Pranay"
+
+# if s == s[::-1]:
+#     print("Palindrome")
+# else:
+#     print("Not Palindrome")
+
+# Factorial
+n = 5
+fact = 1
+
+for i in range(1, n + 1):
+    fact *= i
+
+print(fact)
